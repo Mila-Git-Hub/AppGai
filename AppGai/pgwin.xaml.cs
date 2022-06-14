b34993eb47b00b669d2c69acc416a61474ea7d00
Мила <tkmila@mail.ru>
2022-06-14 15:39:46 +0800

    Добавьте файлы проекта.

---
+++ AppGai/pgwin.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AppGai
+{
+    /// <summary>
+    /// Логика взаимодействия для pgwin.xaml
+    /// </summary>
+    public partial class pgwin : Page
+    {
+        public pgwin()
+        {
+            InitializeComponent();
+            myFrame.Navigate(new ViolationPage());
+        }
+
+        private void ShowViolations(object sender, RoutedEventArgs e)
+        {
+            myFrame.Navigate(new ViolationPage());
+        }
+
+        private void ShowDrivers(object sender, RoutedEventArgs e)
+        {
+            myFrame.Navigate(new DriverPage());
+        }
+
+        private void ShowCars(object sender, RoutedEventArgs e)
+        {
+            myFrame.Navigate(new CarPage());
+        }
+    }
+}
