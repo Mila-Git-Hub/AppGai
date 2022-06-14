2a1fda6a99c9e75e3cdee2a564e33cb6a2be7b90
Мила <tkmila@mail.ru>
2022-06-14 16:38:56 +0800

    Изменен вывод данных о марках(теперь выводится название, а не id марки)

--- AppGai/Car.cs
+++ AppGai/Car.cs
@@ -27,7 +27,9 @@ namespace AppGai
         public string color { get; set; }
         public int madeYear { get; set; }
         public System.DateTime dateOfRegistration { get; set; }
-    
+
+        public string TitleMark { get => Mark1.nameMark; }
+
         public virtual Mark Mark1 { get; set; }
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<DriversCars> DriversCars { get; set; }
